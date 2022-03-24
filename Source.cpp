41b2261891473f5f9557571e035d81a522781e3b
shugaevandrei <66027186+shugaevandrei@users.noreply.github.com>
2022-03-24 22:33:50 +0300

    insertMethod

--- Source.cpp
+++ Source.cpp
@@ -1,3 +1,67 @@
-int main() {
-	return 0;
+#include <iostream>
+#include <thread>
+#include <algorithm>
+#include <mutex>
+
+struct Node
+{
+    int value;
+    Node* next;
+    std::mutex* node_mutex;
+};
+
+class FineGrainedQueue
+{
+    Node* head;
+    int sizeList{ 0 };
+    std::mutex* queue_mutex;
+public:
+    void  insertIntoMiddle(int value, int pos)
+    {
+        Node* prev, * cur;
+        int cnt{ 0 };
+        queue_mutex->lock();
+
+        prev = this->head;
+        cur = this->head->next;
+
+        prev->node_mutex->lock();
+        queue_mutex->unlock();
+
+        cur->node_mutex->lock();
+
+        if (sizeList < pos) {
+            prev->node_mutex->unlock();
+            cur->node_mutex->unlock();
+            cur->value = value;
+            ++sizeList;
+        }
+        else {
+            while (cur)
+            {
+                if (cnt == pos)
+                {
+                    prev->node_mutex->unlock();
+                    cur->node_mutex->unlock();
+                    cur->value = value;
+                    ++sizeList;
+                    return;
+                }
+                ++cnt;
+                Node* old_prev = prev;
+                prev = cur;
+                cur = cur->next;
+                old_prev->node_mutex->unlock();
+                cur->node_mutex->lock();
+            }
+        }
+        prev->node_mutex->unlock();
+    }
+
+};
+
+int main()
+{
+
+    return 0;
 }
